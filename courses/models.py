b1170a938ef7411f5d8dc507ffa9c49d5c055857
Denis Gavrilyuk <karpa4o4@gmail.com>
2021-05-09 14:44:48 +0300

    Implement skeleton

---
+++ courses/models.py
@@ -0,0 +1,23 @@
+"""A module that contains models for the courses application. """
+
+from django.db import models
+
+
+class Course(models.Model):
+    """A class model representing a course. """
+
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Lesson(models.Model):
+    """A class model representing a lesson. """
+
+    name = models.CharField(max_length=64)
+    description = models.TextField(default='')
+    course = models.ForeignKey(Course, models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
