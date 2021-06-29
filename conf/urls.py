6371a244a72efb48a8937a6e4435b836b1d8f3fa
Artem4590 <lavryha4590@gmail.com>
2021-06-29 16:20:11 +0300

    Add preview_image field to Course model

--- conf/urls.py
+++ conf/urls.py
@@ -1,9 +1,11 @@
 """The URL configuration for the project. """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('courses.urls'), name='courses'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
