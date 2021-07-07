a19ff952d857fb889c2eff0db29dd97c0b63561c
Denis Gavrilyuk <karpa4o4@gmail.com>
2021-07-07 17:57:08 +0300

    Configure DEFAULT_AUTHENTICATION_CLASSES

--- conf/settings.py
+++ conf/settings.py
@@ -71,6 +71,9 @@ DATABASES = {
 # Django REST Framework
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ]
